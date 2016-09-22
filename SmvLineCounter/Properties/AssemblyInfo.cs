701cc98119ce069cc4688cff0f2a6b229ff022f3
Rahul Kumar <rahulku@microsoft.com>
2016-09-22 11:15:24 -0700

    demo line counter plugin

---
+++ SmvLineCounter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SmvLineCounter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmvLineCounter")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d4f3a7b-1e75-4904-9ad1-e5ec730981ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SmvLineCounter/SmvLineCounter.cs
@@ -0,0 +1,69 @@
+﻿using SmvLibrary;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmvLineCounter
+{
+    public class SmvLineCounter : ISMVPlugin
+    {
+        string[] extensions = { "*.c", "*.h", "*.cpp", "*.cxx", "*.hxx" };
+
+        private void log(string message)
+        {
+            Log.WriteLog("SmvLineCounter", message);
+        }
+
+        public void Initialize()
+        {
+            log("Initialized");
+        }
+
+        public void PrintPluginHelp()
+        {
+            log("The following arguments need to be provided:");
+            log("\t/configuraiton:<configuration>");
+            log("\t/platform:<platform>");
+        }
+
+        public void ProcessPluginArgument(string[] args)
+        {
+        }
+
+        public void PreAction(SMVAction action) { }
+
+        public void PostAction(SMVAction action)
+        {
+            string sourcesDirectory = Utility.GetSmvVar("sourcesdirectory");
+            int totalLines = 0;
+            log("Line Counts: ");
+            foreach (string extension in extensions)
+            {
+                foreach (string file in Directory.EnumerateFiles(sourcesDirectory, extension, SearchOption.AllDirectories))
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    log(String.Format(CultureInfo.CurrentCulture, "{0}: {1} lines", file, lines.Length.ToString()));
+                    totalLines += lines.Length;
+                }
+            }
+            log("Total: " + totalLines);
+        }
+
+        public void PostBuild(SMVAction[] buildActions)
+        {
+            log("Build completed successfully.");
+        }
+
+        public bool DoPluginAnalysis(SMVAction[] analysisActions)
+        {
+            var results = Utility.ExecuteActions(analysisActions);
+            return Utility.IsExecuteActionsSuccessful(results);
+        }
+
+        public void PostAnalysis(SMVAction[] analysisActions) { }
+    }
+}
