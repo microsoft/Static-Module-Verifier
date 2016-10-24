2965a93fc308e40c7eb6bc6858135fdc7fb4fcc8
Rahul Kumar <rahulku@microsoft.com>
2016-10-24 11:10:58 -0700

    smv service for deploying worker role

---
+++ SmvCloudWorker/CloudConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace SmvLibrary
+{
+    [XmlRootAttribute(Namespace = "")]
+    public class SMVCloudConfig
+    {
+        [XmlElementAttribute("StorageConnectionString")]
+        public StorageConnectionStringElement StorageConnectionString;
+
+        [XmlElementAttribute("ServiceBusConnectionString")]
+        public ServiceBusConnectionStringElement ServiceBusConnectionString;
+    }
+
+    [XmlRootAttribute("StorageConnectionString", Namespace = "")]
+    public class StorageConnectionStringElement
+    {
+        [XmlAttributeAttribute()]
+        public string value { get; set; }
+    }
+
+    [XmlRootAttribute("ServiceBusConnectionString", Namespace = "")]
+    public class ServiceBusConnectionStringElement
+    {
+        [XmlAttributeAttribute()]
+        public string value { get; set; }
+    }
+}
