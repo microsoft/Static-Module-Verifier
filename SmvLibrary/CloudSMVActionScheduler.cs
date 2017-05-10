fcaaf7e1f7b1934a59d1de9def63f7050410c848
Rahul Kumar <rahulku@microsoft.com>
2017-05-10 00:11:42 -0700

    fix error reporting for clou scheduler

--- SmvLibrary/CloudSMVActionScheduler.cs
+++ SmvLibrary/CloudSMVActionScheduler.cs
@@ -225,6 +225,7 @@ namespace SmvLibrary
             {
                 Log.LogError(string.Format("Failed to complete action: {0} ({1})", actionGuid, context.action.name));
                 context.callback(context.action, new SMVActionResult[] { context.action.result }, context.context);
+                Utility.scheduler.errorsEncountered = true;
                 return;
             }
 
