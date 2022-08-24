e0c13a6997520c9691b047c20c735f2d5a830f4c
NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>
2022-08-24 16:57:57 -0700

    Restore accidentally-removed /s.

--- SmvInterceptorWrapper/Program.cs
+++ SmvInterceptorWrapper/Program.cs
@@ -122,10 +122,10 @@ namespace SmvInterceptorWrapper
                 // Persist the RSP file 
                 // Remove file names (*.c) from the content
                 // Ensure we get cases with quotation marks first
-                Regex fileNameRegexQuote1 = new Regex(@"([\s]+("")[\w\p{Pd}\p{Pc}(:|\\).]+\.c(""))", RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                Regex fileNameRegexQuote2 = new Regex(@"([\s]+("")[\w\p{Pd}\p{Pc}(:|\\).]+\.(cpp|cxx)(""))", RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                Regex fileNameRegex1 = new Regex(@"[\s]+[\w\p{Pd}\p{Pc}(:|\\).]+\.c", RegexOptions.IgnoreCase|RegexOptions.Multiline);
-                Regex fileNameRegex2 = new Regex(@"([\s]+[\w\p{Pd}\p{Pc}(:|\\).]+\.(cpp|cxx))", RegexOptions.IgnoreCase|RegexOptions.Multiline);
+                Regex fileNameRegexQuote1 = new Regex(@"([\s]+("")[\w\p{Pd}\p{Pc}(:|\\)\s.]+\.c(""))", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                Regex fileNameRegexQuote2 = new Regex(@"([\s]+("")[\w\p{Pd}\p{Pc}(:|\\)\s.]+\.(cpp|cxx)(""))", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                Regex fileNameRegex1 = new Regex(@"[\s]+[\w\p{Pd}\p{Pc}(:|\\)\s.]+\.c", RegexOptions.IgnoreCase|RegexOptions.Multiline);
+                Regex fileNameRegex2 = new Regex(@"([\s]+[\w\p{Pd}\p{Pc}(:|\\)\s.]+\.(cpp|cxx))", RegexOptions.IgnoreCase|RegexOptions.Multiline);
 
                 List<string> fileNames = new List<string>();
                 int count = 0;
